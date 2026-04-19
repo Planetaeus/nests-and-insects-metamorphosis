837d39d6d5a1ad7da1b99af9fabae2e77b0f4df2
Michael Taft Morris <taft.morris@gmail.com>
2026-04-19 17:43:25 -0400

    Complete rework of Hexes.py
    Pivot from turning this into layers of hexagons and their interconnectedness, and instead into generating concentric layers of ascii hexagons into a grid with coordinate labels and the ability to place an ascii player avatar on a given space.

--- game/src/hexes.py
+++ game/src/hexes.py
@@ -3,84 +3,116 @@ import numpy as np
 import random as rnd
 import math
 
-layers = 30+1
+def double_digits(d):
+    d = d % 100
+    if d < 10:
+        return "0" + str(d)
+    else:
+        return str(d)
+        
+player = ["\\()/",
+          "-()-",
+          "/()\\"]
+          
 
-hex_length = 0
-
-for i in range(layers):
-    for j in range(i*6):
-        hex_length = hex_length + 1
-
-hexes = np.ones([1,hex_length])
-
-rnd.seed()
-
-for i in range(1,hex_length):
-    rnd_int = rnd.randrange(6)
+        
+def place_player(grid,cc,rr):
+    start_line = 0
+    start_index = 0
+    if int((len(grid)-1)/6)-1 % 2 == 1:
+        print("yes")
+        if cc % 2 == 0:
+            start_line = rr * 6 + 3
+            start_index = 6 + 9 * cc - 2
+        elif cc % 2 == 1:
+            start_line = rr * 6
+            start_index = 6 + 9 * cc - 2
+    else:
+        if cc % 2 == 0:
+            start_line = rr * 6
+            start_index = 6 + 9 * cc - 2
+        elif cc % 2 == 1:
+            print("Yes?")
+            start_line = rr * 6 + 3
+            start_index = 6 + 9 * cc - 2
     
-    if rnd_int > 2:
-        hexes[0,i] = 0
-        
+    for (l,p) in zip(range(start_line, start_line + 3),player):
+        line = grid[l]
+        line = line[0:start_index] + p + line[start_index+4:]
+        grid[l] = line
+    return grid
+    
 
-fig, ax = plt.subplots()
-good_hex = np.zeros([1,4])
+layers = 5
+ascii_hex = ["   ______   ",
+             "  / CCRR \\  ",
+             " /        \\ ",
+             "/          \\",
+             "\\          /",
+             " \\        / ",
+             "  \\______/  "]
+# Hexagon courtesy of https://ascii.co.uk/art/hexagon
 
-index = 0
-for i in range(layers):
-    for j in range(i*6):
-        if hexes[0,index] == 1:
-            radius = i
-            angle = j * 2 * math.pi / (i * 6)
+blank_space = 9 * " "
+for line in ascii_hex:
+    print(line)
+    
+hex_grid = [""] * (1 + (len(ascii_hex)-1) * (2*layers + 1))
+layer_tracker = (layers + 1) * [-1]
+layer_tracker[0] = 0
+for i in range(int(len(hex_grid)/2)+1):
+    space = ""
+    top_row = ""
+    bot_row = ""
+    top_left = ""
+    bot_left = ""
+    top_right = ""
+    bot_right = ""
+    for l in range(len(layer_tracker)):
+        layer = layer_tracker[l]
+        if l == 0:
+            top_row = ascii_hex[layer]
+            bot_row = ascii_hex[-layer]
+            layer_tracker[l] = (layer % 6) + 1
+        elif layer > -1 and l > 0:
+            layer_tracker[l] = (layer % 6) + 1
+            layer = layer_tracker[l]
+            top_left = ascii_hex[layer][:9] + top_left
+            bot_left = ascii_hex[-layer][:9] + bot_left
+            top_right = top_right + ascii_hex[layer][3:]
+            bot_right = bot_right + ascii_hex[-layer][3:]
+
+    if i <= layers * 3:
+        space = (layers - math.floor(i/3)) * blank_space
+        if i > 0 and i % 3 == 0:
+            layer = int(i / 3)
+            layer_tracker[layer] = 0
+            top_left = ascii_hex[0][:9] + top_left
+            bot_left = blank_space + bot_left
+            top_right = top_right + ascii_hex[0][3:]
+            bot_right = bot_right + blank_space
+
+    hex_grid[i] = space + top_left + top_row + top_right + space
+    hex_grid[-i] = space + bot_left + bot_row + bot_right + space
+
+for i in range(len(hex_grid)):
+    line = hex_grid[i]
+    if i % 3 == 1:
+        line = line.split("CCRR")
+        l = line[0]
+        cc = int((len(l) - 4) / 9)
+        rr = 0
+        rr = math.floor(i/6)
+        for s in line[1:]:
+            l = l + double_digits(cc) + double_digits(rr) + s
+            cc = cc + 2
+        line = l
+        hex_grid[i] = line
+                
+    print(line)
+    
+new_grid = place_player(hex_grid,7,6)
+
+for line in new_grid:
+    print(line)
             
-            x = i * np.cos(angle)
-            y = i * np.sin(angle)
-            
-            hex = np.zeros([1,4])
-            hex[0,0] = x
-            hex[0,1] = y
-            hex[0,2] = i
-            hex[0,3] = j
-            good_hex = np.append(good_hex,hex,axis = 0)
-            
-            #ax.annotate(str(10-i) + "," + str(j+1),(x,y))
-        index = index + 1
-
-edges = []
-
-for i in range(np.size(good_hex,axis = 0)):
-    h1 = good_hex[i,:]
-    for j in range(i+1, np.size(good_hex,axis = 0)):
-        h2 = good_hex[j,:]
-        i1 = h1[2]
-        i2 = h2[2]
-        j1 = h1[3]
-        j2 = h2[3]
-        plot = False
-        if i2 == i1:
-            if j2 - j1 == 1:
-                plot = True
-            elif j2 - j1 == i2 * 6-1:
-                plot = True
-        elif i2 - i1 == 1:
-            s2 = int(j2 / i2)
-            s1 = s2
-            if i1 != 0:
-                s1 = int(j1 / i1)
-            
-            if j2 - j1 == s2:
-                plot = True
-            elif j2 - j1 == i2 * 6 - 1:
-                plot = True
-            elif s1 == s2:
-                if j2 % i2 - j1 % i1 == 1:
-                    plot = True
-        elif i2 - i1 > 1:
-            break
-        if plot == True:
-            ax.plot([h1[0],h2[0]],[h1[1],h2[1]])
-            edges.append([h1[2:3],h2[2:3]])
-                    
-ax.scatter(good_hex[:,0],good_hex[:,1])
-plt.show()
-
-        
