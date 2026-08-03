b66a26b21ec05b6b630fe1fe69b53dedd795ee98
Michael Taft Morris <taft.morris@gmail.com>
2026-08-03 19:40:17 -0400

    Rulebook Formatting
    Created rulebook_formatting.py and New_Rulebook_Raw.txt
    rulebook_formatting.py takes the New_Rulebook_Raw.txt and parses it and formats it into Nests_And_Insects_Metamorphosis.txt, which is also a new file.
    Nests_And_Insects_Metamorphosis.txt is intended to be representative of a final copy of the book for distribution. It is by no means a finished product yet, but it resembles my goal for the finished product.

---
+++ game/rulebook_formatting.py
@@ -0,0 +1,211 @@
+# Imports
+
+raw_filename = 'New_Rulebook_Raw.txt'
+out_filename = 'Nests_And_Insects_Metamorphosis.txt'
+
+page_header = '╔══► Nests & Insects: Metamorphosis ◄═════════════════════════════════════════► Rulebook ◄══╗'
+page_footer_left = '╚═══════════════════════════════════════════'
+page_footer_right = '═══════════════════════════════════════════╝'
+margin_left = '║ '
+margin_right = ' ║'
+page_width = 89
+line_break = '─' * page_width
+
+def update_header(headers, rl):
+    hs = headers
+    if '/h1' in rl: # Header 1
+        hs[0] = hs[0] + 1
+        hs[1] = 0
+        hs[2] = 0
+        hs[3] = 0
+    elif '/h2' in rl: # Header 2
+        hs[1] = hs[1] + 1
+        hs[2] = 0
+        hs[3] = 0
+    elif '/h3' in rl: # Header 3
+        hs[2] = hs[2] + 1
+        hs[3] = 0
+    elif '/h4' in rl: # Header 4
+        hs[3] = hs[3] + 1
+    else:
+        hs = [0,0,0,0]
+    
+    return hs
+     
+def header_to_string(hs):
+    return str(hs[0]) + '.' + str(hs[1]) + '.' + str(hs[2]) + '.' + str(hs[3])
+    
+def format_header(headers, rl):
+    hs = update_header(headers, rl)
+
+    hs_string = header_to_string(hs)
+    
+    wl = hs_string + rl[3:]
+    
+    return hs, wl
+    
+def pad_right(s, target_width):
+    len_diff = target_width - len(s)
+    if len_diff > 0:
+        s = s + len_diff * ' '
+    
+    return s
+
+def pads_right(lines, target_width):
+    out_line = []
+    if isinstance(lines, str):
+        out_line = pad_right(lines, target_width)
+    else:
+        for line in lines:
+            out_line.append(pad_right(line, target_width))
+    
+    return out_line
+        
+def add_margins(s):
+    return margin_left + s + margin_right
+        
+def adds_margins(lines):
+    out_line = []
+    if isinstance(lines, str):
+        out_line = add_margins(lines)
+    else:
+        for line in lines:
+            out_line.append(add_margins(line))
+    
+    return out_line
+
+def format_cols(columns):
+    cols = columns.split('|')
+    for i in range(len(cols)):
+        col = cols[i].split(';')
+        col_w = 0
+        for j in range(len(col)):
+            if len(col[j]) > col_w:
+                col_w = len(col[j])
+        for j in range(len(col)):
+            col[j] = pad_right(col[j], col_w)
+            
+        cols[i] = col
+    col_num = len(cols)
+    row_num = len(cols[0])
+    rows = [''] * row_num
+    for row in range(row_num):
+        for col in range(col_num):
+            rows[row] = rows[row] + ' | ' + cols[col][row]
+        rows[row] = rows[row] + ' | '
+    
+    out_rows = (row_num + 3) * ['']
+    out_rows[0] = line_break
+    out_rows[1] = rows[0]
+    out_rows[2] = line_break
+    for i in range(1,row_num):
+        out_rows[i+2] = rows[i]
+    out_rows[-1] = line_break
+    
+    return out_rows
+
+def format_table_header(table_num, rl):
+    tn = table_num + 1
+    wl = 'Table ' + str(tn) + ': ' + rl[3:]
+    
+    return tn, wl
+    
+def trim_line(lines):
+    out_lines = ['']
+    col = 0
+    line_index = 0
+    if isinstance(lines,str):
+        l = lines.strip()
+        words = l.split()
+        for word in words:
+            if col + 1 + len(word) >= 89:
+                out_lines[line_index] = out_lines[line_index]
+                col = len(word)
+                line_index = line_index + 1
+                out_lines.append(word)
+            elif col == 0:
+                col = len(word)
+                out_lines[line_index] = word
+            else:
+                col = col + 1 + len(word)
+                out_lines[line_index] = out_lines[line_index] + ' ' + word
+            
+            out_lines[line_index].strip()
+    else:
+        for line in lines:
+            l = line.strip()
+            out_lines.append(l)
+    
+    return out_lines
+    
+def format_line(hs, tn, rl):
+    wl = ''
+    rl = rl.strip()
+    if len(rl) > 0:
+        if '/' in rl[0]: # Is formatting character present?
+            if 'h' in rl[1]:
+                hs, wl = format_header(hs, rl)
+            elif 't' in rl[1]:
+                tn, wl = format_table_header(tn, rl)
+            elif '/cols' in rl:
+                wl = format_cols(rl[6:])
+        else:
+            wl = rl.strip()
+    wl = trim_line(wl)
+    wl = pads_right(wl, page_width)
+    wl = adds_margins(wl)
+    return hs, tn, wl
+
+def format_page_footer(page_num):
+    num_string = ''
+    if page_num / 10 < 1:
+        num_string = '--' + str(page_num) + '--'
+    elif page_num / 100 < 1:
+        num_string = '--' + str(page_num) + '-'
+    elif page_num / 1000 < 1:
+        num_string = '-' + str(page_num) + '-'
+        
+    return page_footer_left + num_string + page_footer_right
+
+with open(raw_filename, 'r', encoding='utf-8') as raw_file, open(out_filename, 'w', encoding='utf-8') as out_file:
+    read_line = '' 
+    write_line = ''
+    line_index = 0
+    lines_read = 0
+    page_index = 0
+    headers = [0,0,0,0]
+    table_num = 0
+    
+    while True:
+        
+        read_line = raw_file.readline()
+        lines_read = lines_read + 1
+        if read_line == '': # Is the string empty?
+            break
+        else:
+            headers, table_num, write_lines = format_line(headers, table_num, read_line)
+            for wl in write_lines:
+                write_line = wl
+                
+                if line_index % 48 == 0:
+                    out_file.write(page_header + u'\n')
+                    
+                out_file.write(write_line + u'\n')
+                
+                if line_index % 48 == 47:
+                    page_index = page_index + 1
+                    footer = format_page_footer(page_index)
+                    out_file.write(footer + u'\n')
+                
+                line_index = line_index + 1    
+    write_line = margin_left + page_width * ' ' + margin_right
+
+    while line_index % 48 != 0:
+        out_file.write(write_line + u'\n')
+        if line_index % 48 == 47:
+            page_index = page_index + 1
+            footer = format_page_footer(page_index)
+            out_file.write(footer + u'\n')
+        line_index = line_index + 1
+    
+       
